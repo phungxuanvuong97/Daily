5f87ef43dea0b575725c72a79d9bc785d1ea8623
Phung Xuan Vuong <pxvuong@cmcglobal.vn>
2023-09-10 14:37:23 +0700

    fix typo

--- coding/common/data_structure/list/List.c
+++ coding/common/data_structure/list/List.c
@@ -3,3 +3,23 @@
 //
 
 #include "List.h"
+List* CreateList(){
+    List *list = malloc(sizeof(List));
+    list->length = 0;
+    return list;
+}
+
+void AddToList(List *list, int value){
+
+    int newLength = list->length + 1;
+    int *values = realloc(list->values, sizeof(int) * newLength);
+    values[newLength - 1] = value;
+
+    if(!list->values){
+        int* oldValues = list->values;
+        free(oldValues);
+    }
+
+    list->values = values;
+    list->length = list->length + 1;
+}
--- coding/common/data_structure/list/List.h
+++ coding/common/data_structure/list/List.h
@@ -14,28 +14,7 @@ struct ListNodeItem{
 
 typedef struct ListNodeItem List;
 
-List* CreateList(){
-    List *list = malloc(sizeof(List));
-    list->length = 0;
-    return list;
-}
-
-void AddToList(List *list, int value){
-
-    int newLength = list->length + 1;
-    int *values = realloc(list->values, sizeof(int) * newLength);
-    values[newLength - 1] = value;
-
-    if(!list->values){
-        int* oldValues = list->values;
-        free(oldValues);
-    }
-
-    list->values = values;
-    list->length = list->length + 1;
-}
-
-
-
+List* CreateList();
+void AddToList(List *list, int value);
 
 #endif //IOTLEETCODE_LIST_H
